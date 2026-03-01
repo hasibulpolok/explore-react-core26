b7b575c9aa50cbd9a3af01f942286644bc9a3f45
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-03-02 03:28:00 +0600

    destructure in array

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,81 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-
-
+import './App.css';
 
 const Apps = () => {
-
   return (
     <>
       <Person />
       <Student />
-      <Developer name="Polok"  tech="JS"/>
-      <Developer name="Ridu"  tech="Laravel"/>
+      <Developer name="Polok" tech="JS" />
+      <Developer name="Ridu" tech="Laravel" />
       <Developer name="Tanjil" tech="Laravel" />
+      <Player name="Rana" sports="Football" />
+      <Player name="Roni" sports="Cricket" />
     </>
-  )
-}
+  );
+};
 
 const Student = () => {
   return (
-    <div className='student'>
-      <h2>  name:</h2>
-      <h2> department:</h2>
+    <div className="student">
+      <h2>Name:</h2>
+      <h2>Department:</h2>
     </div>
-  )
+  );
 };
-const Person = () => {
 
+const Person = () => {
   const age = 28;
   const name = "Polok";
-  const personStyle ={
-    color:'red',
-    fontSize:'50px'
-  }
-  return (
-    <>
-      <p style={personStyle}>I'm {name}. I'm {age} years old</p>
-    </>
-  )
-}
 
-const Developer =(props)=>{
-   const {name,tech} = props;
-  return(
-   <>
-  <div style={{ border: '2px solid green', margin: '10px 10px 10px 10px' , padding:'0 10px 0 10px' , borderRadius:'20px' }}>
-     <h2>Developer {name}</h2>
-   <h2>Technology {tech}</h2>
-  </div>
-   </>
-  )
-}
+  const personStyle = {
+    color: "red",
+    fontSize: "50px",
+  };
+
+  return (
+    <p style={personStyle}>
+      I'm {name}. I'm {age} years old.
+    </p>
+  );
+};
+
+const Developer = (props) => {
+  const { name, tech } = props;
+
+  return (
+    <div
+      style={{
+        border: "2px solid green",
+        margin: "10px",
+        padding: "10px",
+        borderRadius: "20px",
+      }}
+    >
+      <h2>Developer: {name}</h2>
+      <h2>Technology: {tech}</h2>
+    </div>
+  );
+};
+
+const Player = (props) => {
+  const { name, sports } = props;
+
+  return (
+    <div
+      style={{
+        border: "2px solid blue",
+        margin: "10px",
+        padding: "10px",
+        borderRadius: "20px",
+      }}
+    >
+      <h2>Player Name: {name}</h2>
+      <p>Sports: {sports}</p>
+    </div>
+  );
+};
 
 export default Apps;
